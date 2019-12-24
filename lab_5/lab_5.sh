7a3b2d49abceec861911defa644b0d4064eb9f91
Sergey K <43613813+yerseg@users.noreply.github.com>
2019-12-24 15:05:32 +0300

    Update script

--- lab_5/lab_5.sh
+++ lab_5/lab_5.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-if [ $1 == $2 ]; then 
-	echo "$3"
-fi
+ps -eo euid,ruid,comm | tail -n +2 >temp.out #tail чтобы не учитывать строку "EUID RUID COMMAND"
+exec 0<temp.out
+while read euid ruid command
+do
+	if [[ $euid != $ruid ]]; then
+		echo "process $command's euid differs from ruid"
+	fi
+done
+rm temp.out
